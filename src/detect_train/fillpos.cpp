7421cffab4de6dbd7d2ea89f3bb16971d7476b89
Double <double0101@foxmail.com>
2018-01-28 19:17:23 +0800

    fsf

--- src/detect_train/fillpos.cpp
+++ src/detect_train/fillpos.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <cstdio>
 #include <fstream>
+#include <string>
 
-#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+#include "sample.hpp"
 
 using namespace cv;
 using namespace std;
 
-
+void parsesample(const string&);
+void sampleimgproc(SampleInfo&);
 
 int main(int argc, const char** argv)
 {
@@ -16,8 +19,31 @@ int main(int argc, const char** argv)
 
 	while (getline(input, s))
 	{
-		cout << s << endl;
+		parsesample(s);
 	}
 	input.close();
 	return 0;
+}
+
+void parsesample(const string& s)
+{
+	SampleInfo sample(s);
+	sampleimgproc(sample);
+}
+
+void sampleimgproc(SampleInfo& possample)
+{
+	Mat img = imread(possample.getpath(), 1);
+	vector<vector<int>> rects = possample.getRects();
+	for (int i = 0; i < possample.getnum(); ++i)
+	{
+		rectangle(img, 
+				Rect(rects[i][0],
+					 rects[i][1],
+					 rects[i][2],
+					 rects[i][3]),
+				Scalar(0, 0, 0),
+				CV_FILLED, 8, 0);
+	}
+	imwrite(possample.getpath(), img);
 }
--- src/detect_train/sample.cpp
+++
@@ -1,8 +0,0 @@
-#include <sstream>
-#include "sample.hpp"
-SampleInfo::SampleInfo(const string &info) {
-	std::istringstream isstr(info);
-	isstr >> m_filepath;
-	isstr >> m_num;
-
-}
