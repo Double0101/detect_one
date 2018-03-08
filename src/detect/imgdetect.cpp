c2854d1f016da1a1c8c5781f5fe19861a3129c80
Double <double0101@foxmail.com>
2018-03-08 21:54:58 +0800

    add filter

--- src/detect/imgdetect.cpp
+++ src/detect/imgdetect.cpp
@@ -8,43 +8,48 @@ using namespace cv;
 
 int checkColor(Mat& frame);
 void detectAndDisplay(Mat& frame);
+void detect(Mat frame, Mat& frame_gray, int flags);
 
 CascadeClassifier obj_cascade;
 
 string window_name = "image detect";
 string obj_classifier_name = "/Users/Double/workspace/Projects/cheguo/detect_one/build/bin/data/cascade.xml";
 
+const int DETECTWITHCOLOR = 1;
+
 int main(int argc, const char** argv)
 {
-    Mat frame = imread( argv[1], 1 );
+    Mat src = imread( argv[1], 1 );
     if ( argc > 2 ) { obj_classifier_name = argv[2]; }
     if ( !obj_cascade.load( obj_classifier_name ) ) { printf("--(!)Error loading cascade\n"); return -1; }
-    detectAndDisplay( frame );
+
+	// TODO scale frame
+	double scale = min(1000.f / src.cols, 1000.f / src.rows);
+	resize(src, src, Size(src.cols * scale, src.rows * scale));
+    detectAndDisplay( src );
     return 0;
 }
 
 void detectAndDisplay(Mat& frame)
 {
-    vector<Rect> objs;
     Mat frame_gray;
     cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
     equalizeHist( frame_gray, frame_gray );
-
-    obj_cascade.detectMultiScale( frame_gray, objs, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-    for ( size_t i = 0; i < objs.size(); ++i )
+    Mat frame_copy = frame.clone();
+    detect(frame, frame_gray, 0);
+    while (1)
     {
-        Mat roi( frame, objs[i] );
-        // TODO color filter
-        if ( checkColor( roi ) == 1 )
+        char key = waitKey(10);
+        if (key == 'c')
         {
-            Point center(objs[i].x + objs[i].width / 2, objs[i].y + objs[i].height / 2);
-            ellipse(frame, center, Size(objs[i].width / 2, objs[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+            cout << "detect with color filter" << endl;
+            destroyWindow(window_name);
+            detect(frame_copy, frame_gray, 1);
+            break;
         }
+        if (key == 'q') break;
     }
-    imshow( window_name, frame );
     waitKey(0);
-
 }
 
 int checkColor(Mat& frame)
@@ -71,4 +76,44 @@ int checkColor(Mat& frame)
                 ++count;
     }
     return ( (float) count / ( num_col * num_row ) > 0.11f ) ? 1 : 0;
+}
+
+void detect(Mat frame, Mat& frame_gray, int flags)
+{
+    vector<Rect> objs;
+    obj_cascade.detectMultiScale( frame_gray, objs, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+
+    if (flags == DETECTWITHCOLOR)
+    {
+        for (size_t i = 0; i < objs.size(); ++i)
+        {
+            // TODO size filter
+            if (objs[i].width <= 100)
+            {
+                Mat roi(frame, objs[i]);
+                if (checkColor(roi) == 1)
+                {
+                    Point center(objs[i].x + objs[i].width / 2, objs[i].y + objs[i].height / 2);
+                    ellipse(frame, center, Size(objs[i].width / 2, objs[i].height / 2),
+                            0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+                }
+            }
+        }
+    }
+    else
+    {
+        for (size_t i = 0; i < objs.size(); ++i)
+        {
+            // TODO size filter
+            if (objs[i].width <= 100)
+            {
+                Mat roi(frame, objs[i]);
+                Point center(objs[i].x + objs[i].width / 2, objs[i].y + objs[i].height / 2);
+                ellipse(frame, center, Size(objs[i].width / 2, objs[i].height / 2),
+                        0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+            }
+        }
+    }
+
+    imshow(window_name, frame);
 }
